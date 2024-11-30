0e045b54600c56f64b82fdaf1443d1211d462e44
NadavEyran <nadav.eyran@post.runi.ac.il>
2024-11-30 12:34:29 +0200

    Q1 02

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -163,7 +163,9 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-    // בדיקת קלטים: אם אחת המחרוזות היא null, מחזירים -2
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+
     if (str1 == null || str2 == null) {
         return -2;
     }
