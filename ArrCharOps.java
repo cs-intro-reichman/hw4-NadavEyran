473a37a4d8596f6196d7e259338b9bb07e9aabf2
NadavEyran <nadav.eyran@post.runi.ac.il>
2024-12-01 22:15:02 +0200

    Q1 06

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -163,7 +163,7 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-    if (str1 == null || str2 == null) {
+    if (str1 == "" || str2 == "") {
         return -2;
     }
 
