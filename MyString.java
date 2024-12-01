282e138d365f8559680ce60bd6dcddd0c0834de6
NadavEyran <nadav.eyran@post.runi.ac.il>
2024-12-01 23:42:18 +0200

    Q3

--- MyString.java
+++ MyString.java
@@ -18,13 +18,34 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newString = "";
+        for (int i = 0; i<str.length(); i++ ) {
+            char currenthar = str.charAt(i);
+
+            if (currenthar > 'A'  && currenthar < 'Z') {
+                currenthar = (char) (currenthar + 32);
+            }
+            newString = newString+currenthar;
+        }
+        return newString;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         // Replace the following statement with your code
+        char oneStr2 = str2.charAt(0);
+        int lengthStr2 = str2.length();
+
+        if (str1.indexOf(oneStr2) != -1) {
+            boolean isSubstring = true;
+            for (int i = 0; i < lengthStr2; i++) {
+                if (str1.indexOf(str2.charAt(i)) == -1) {
+                    isSubstring = false;
+                    break;
+                }
+            } 
+            return isSubstring; 
+        }
         return false;
     }
 }
