197629386995da67d7e165846bd82aae4fc10615
NadavEyran <nadav.eyran@post.runi.ac.il>
2024-12-04 16:32:51 +0200

    q4

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -17,10 +17,60 @@ public class KeywordsDetector {
         String[] keywords = {"synergy", "disrupt", "leverage", "Paradigm", "transform"};
         detectAndPrint(sentences, keywords);
     }
+    // If str1 contains str2, returns true; otherwise returns false. */
+    public static boolean contains(String str1, String str2) {
+        // Replace the following statement with your code
+        if (str2.length() == 0) {
+            return true;
+        }
+
+        if (str1.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            boolean isSubstring = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    isSubstring = false;
+                    break;
+                }
+            }
+            if (isSubstring) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Returns the lowercase version of the given string. */
+    public static String lowerCase(String str) {
+        String newString = "";
+        for (int i = 0; i<str.length(); i++ ) {
+            char currenthar = str.charAt(i);
+
+            if (currenthar >= 'A'  && currenthar <= 'Z') {
+                currenthar = (char) (currenthar + 32);
+            }
+            newString = newString+currenthar;
+        }
+        return newString;
+    }
 
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
         // Replace this comment with your code
+        String sentence;
+        String world;
+        for (int i = 0 ; i < sentences.length ; i++){
+            sentence = lowerCase(sentences[i]);
+            for(int j = 0 ; j < keywords.length; j++){
+                world = lowerCase(keywords[j]);
+                if(contains(sentence, world) == true){
+                    System.out.println(sentences[i]);
+                }
+            }
+        }
     }
 }
